44583474c57aa70f02ec205742dab140e4c67305
jmternes <68615887+jmternes@users.noreply.github.com>
2022-09-08 14:43:42 -0500

    Add project files.

---
+++ VisualStudioAssignment/wwwroot/js/site.js
@@ -0,0 +1,11 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+function changeCircle() {
+    document.getElementById("circle").style.backgroundColor = "red";
+}
+
+function normalCircle() {
+    document.getElementById("circle").style.backgroundColor = "blue";
+}
